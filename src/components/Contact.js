91bcaeb559694e9355ab14ab841f74e58440f8be
Daniel Rooks <danrooks7@gmail.com>
2018-07-05 20:24:07 -0400

    Contact Update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,23 +28,23 @@ const Contact = (props) => (
                     <div className="contact-method">
                         <span className="icon alt fa-envelope"></span>
                         <h3>Email</h3>
-                        <a href="#">information@untitled.tld</a>
+                        <a href="#">hello@deltahacks.net</a>
                     </div>
                 </section>
                 <section>
                     <div className="contact-method">
                         <span className="icon alt fa-phone"></span>
                         <h3>Phone</h3>
-                        <span>(000) 000-0000 x12387</span>
+                        <span>Is there a DH phone?</span>
                     </div>
                 </section>
                 <section>
                     <div className="contact-method">
                         <span className="icon alt fa-home"></span>
                         <h3>Address</h3>
-                        <span>1234 Somewhere Road #5432<br />
-                        Nashville, TN 00000<br />
-                        United States of America</span>
+                        <span> 1280 Main St. <br />
+                        Hamilton, ON <br />
+                        Canada</span>
                     </div>
                 </section>
             </section>
