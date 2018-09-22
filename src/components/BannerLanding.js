46fca283945d4f1f1abc789828fd1b384f4a1847
Abhayraj Jain <abhay@abhayraj.net>
2018-09-22 11:51:57 -0400

    Big apply button

--- src/components/BannerLanding.js
+++ src/components/BannerLanding.js
@@ -9,8 +9,8 @@ const BannerLanding = (props) => (
 
             <div className="content">
                 <p>Jan 26-27, 2019   /   McMaster University</p>
-                <a href="/apply" className="button special">Join Us</a>
                 <a href="#contact" className="button scrolly">Contact Us</a>
+                <a href="https://deltahacks.us19.list-manage.com/subscribe/post?u=7ac221f02edc4242db4789d8f&amp;id=7f28a270ff" className="button">Mailing List</a>
                 {/* <button>Scroll</button> */}
                 <br />
                 <ul className="icons">
@@ -22,23 +22,25 @@ const BannerLanding = (props) => (
                 <br />
             </div>
 
-            <form action="https://deltahacks.us19.list-manage.com/subscribe/post?u=7ac221f02edc4242db4789d8f&amp;id=7f28a270ff" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            {/* <form action="https://deltahacks.us19.list-manage.com/subscribe/post?u=7ac221f02edc4242db4789d8f&amp;id=7f28a270ff" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                 <input type="hidden" name="bot-field" />
                 <div className="row uniform">
-                    {/* <div className="6u 12u(xsmall)">
+                    <div className="6u 12u(xsmall)">
                         <label><input type="text" name="NAME" placeholder="Name"/></label>   
                     </div>
                     <div className="6u 12u(xsmall)">
                         <label><input type="email" name="EMAIL" id="email" placeholder="Email" /></label>
-                    </div> */}
+                    </div>
                     <div className="12u">
                         <ul className="actions">
                             <li><input type="submit" value="Join Mailing List" /></li>
-                            {/* <li><input type="reset" value="Reset" /></li> */}
+                            <li><input type="reset" value="Reset" /></li>
                         </ul>
                     </div>
                 </div>
-            </form>
+            </form> */}
+            <a href="/apply" className="12u button special">Apply Now</a>
+
 
         </div>
     </section>
