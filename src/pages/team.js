94101d5ec8f46bbe50ca96ba6d22f73835dc5529
Maanav Garg <maanav@Maanavs-MacBook-Pro-2.local>
2018-10-20 06:01:25 -0400

    team page added

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,8 +1,29 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
+import '../assets/css/team.css'
+import YouTube from 'react-youtube'
 
-import pic11 from '../assets/images/pic11.jpg'
+import test from '../assets/images/test.png'
+
+const BGvideoOptions = {
+    height: '500',
+    width: '100%',
+    playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay:1,
+        controls: 0,
+        rel: 0,
+        showinfo: 0,
+        modestbranding: 1,  // Hide the Youtube Logo
+        loop: 1,            // Run the video in a loop - DOESN'T WORK - used onEnd function
+        fs: 0,              // Hide the full screen button
+        cc_load_policy: 0, // Hide closed captions
+        iv_load_policy: 3,  // Hide the Video Annotations
+        autohide: 0,
+        enablejsapi: 0
+    }
+};
+
 
 const Team = (props) => (
     <div>
@@ -11,16 +32,203 @@ const Team = (props) => (
             <meta name="description" content="Generic Page" />
         </Helmet>
 
+                   {/* Code for background video below this*/}
+                   <div className="video-background">
+                <div className="video-foreground">
+                    <YouTube
+                        videoId="3SizoIuIedc"
+                        opts={BGvideoOptions}
+                        className="video-iframe"
+                        onReady={function(e) {
+                            e.target.mute();
+                          }}
+                        onEnd={function(e) {
+                            e.target.playVideo();
+                        }}
+                    />
+                </div>
+            </div>
+            {/* Code for background video ends here */}
+
         <div id="main" className="alt">
             <section id="one">
                 <div className="inner">
                     <header className="major">
-                        <h1>Generic</h1>
+                        <h1>Our Team</h1>
                     </header>
-                    <span className="image main"><img src={pic11} alt="" /></span>
-                    <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
-                    <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
+                <div className="grid">
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Co-Director
+                            </p>
+                    </div>
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Co-Director
+                            </p>
+                    </div>
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Co-Director
+                            </p>
+                    </div>
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Head of Sponsorship
+                            </p>
+                    </div>
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Software Developer
+                            </p>
+                    </div>
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Website Developer
+                            </p>
+                    </div>
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Software Developer
+                            </p>
+                    </div>
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Website Developer
+                            </p>
+                    </div>
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Software Developer
+                            </p>
+                    </div>
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Website Developer
+                            </p>
+                    </div>
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Software Developer
+                            </p>
+                    </div>
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Website Developer
+                            </p>
+                    </div>
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Software Developer
+                            </p>
+                    </div>
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Website Developer
+                            </p>
+                    </div>
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Software Developer
+                            </p>
+                    </div>
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Website Developer
+                            </p>
+                    </div>
+
+                    <div className="each">
+                        <img src={test}/>
+                            <p className="name">
+                                Maanav Garg
+                            </p>
+                            <p className="bio">
+                                Website Developer
+                            </p>
+                    </div>
+
+                </div>   
                 </div>
             </section>
         </div>
