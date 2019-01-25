176ee7e334268aa9643de58b1be188919385d3b4
Rumsha <rumsha7@gmail.com>
2019-01-25 03:00:39 -0500

    Sponsor challenges

--- src/pages/sponsorchallenges.js
+++ src/pages/sponsorchallenges.js
@@ -76,23 +76,50 @@ const SponsorChallenges = (props) => (
                         <p>In addition to the loss of the value of the goods stolen, businesses also suffer secondary financial losses due to increased insurance costs, increased security costs, increased technology costs to track assets, and loss of productivity when trucks are stolen and unavailable for use. There is also a safety issue for drivers when a cargo theft happens with a “live” load on the road.</p>
                     </AccordionItemBody>
                     <AccordionItemBody>
-                        <p>For more information, contact Ezzeldin Tahoun on Slack or by email: tahoune@mcmaster.ca </p>
+                        <b>Potential Solution:</b>
+                        <p>While most medium- to large-scale trucking companies have tractors and trailers equipped with GPS tracking devices, these are often only useful in recovering the stolen tractor/trailer/goods well after the theft has taken place and been reported.</p>
+                        <p>However, an opportunity exists to create a “theft detection” app that will provide a real time alert to company management that a theft may be in progress in its early stages. Using artificial intelligence, the app could identify if a tractor or trailer equipped with GPS is in motion during a time period that is outside the norm, based on historical GPS data. For example, most thefts take place on weekends. The app would detect if a trailer that typically works on Mondays to Fridays is in motion on a Saturday, and then alert management that there may be a theft in progress.</p>
+                        <p>This app would benefit businesses by giving them precious time to alert police, which would increase the chances of recovering the goods, minimize possible damage to the equipment, and increase the chances of apprehending the thieves. As cargo theft arrests increase, the number of incidents will decrease as organized crime moves on to easier theft opportunities. Insurance premiums would decrease for companies as their security profile improves.</p>
+                    </AccordionItemBody>
+                    <AccordionItemBody>
+                        <b>Potential Customers:</b>
+                        <ul>
+                            <li>Trucking and logistics companies</li>
+                            <li>Companies with their own fleet of trucks</li>
+                            <li>Insurance companies</li>
+                            <li>Alarm and security companies</li>
+                        </ul>
                     </AccordionItemBody>
                 </AccordionItem>
                 <AccordionItem>
                     <AccordionItemTitle>
-                        <p>+ Wifi Security Hack - EzSec</p>
+                        <p>+ McMaster Materials Department - The Ultimate Image Analysis Software</p>
                     </AccordionItemTitle>
                     <AccordionItemBody>
-                        <p></p>    
+                        <p>The properties of materials depend on the way in which atoms and defects are arranged in the material.  For this reason, materials engineers observe the material using optical and electron microscopes.  The images they take are often like attached figures.  What we would like you to do, is to measure the fractions of the light and dark “phases” in these figures.  The fraction could simply be expressed as number of pixels of a given color over the total number of pixels. The problem is complicated by the lighting conditions.  Sometimes you’ll get very good contrast between the phases (e.g. micro1.png) and sometimes there is less contrast.  In addition, the light and dark phases may contain “particles” like those shown in micro2.png.  You will be awarded (25) points for achieving each of the targets listed below as well as (50) points for the user interface and ease of use.</p>    
                     </AccordionItemBody>
                     <AccordionItemBody>
-                        <p>For more information, contact Ezzeldin Tahoun on Slack or by email: tahoune@mcmaster.ca </p>
+                        <p>Target 1:  Measure the fraction of the dark phase with an accuracy of 0.5% or better in an image that does not contain particles (e.g. micro1.png).</p>
+                    </AccordionItemBody>
+                    <AccordionItemBody>
+                        <p>Target 2:  Measure the fraction of the dark phase with an accuracy of 0.5% or better in an image that contains particles (e.g. micro3.png).  For images with particles, the particles inside the dark phase should be counted with the dark phase and the ones inside the light phase should be counted with the light phase.</p>
+                    </AccordionItemBody>
+                    <AccordionItemBody>
+                        <p>Target 3:  Same as target 2, but you will be given images with uneven lighting conditions.</p>
+                    </AccordionItemBody>
+                    <AccordionItemBody>
+                        <p>Target 4:  You need to measure the average size (equivalent diameter) of the grains in the images.  The “grains” simply refer to the polygons that make the images.</p>
+                    </AccordionItemBody>
+                    <AccordionItemBody>
+                        <p>Target 5:  You are able to produce a histogram of the grain size.</p>
+                    </AccordionItemBody>
+                    <AccordionItemBody>
+                        <p>On the day of the challenge, you will be provided with over 300 images to practice on.</p>
                     </AccordionItemBody>
                 </AccordionItem>
                 <AccordionItem>
                     <AccordionItemTitle>
-                        <p>+ Wifi Security Hack - EzSec</p>
+                        <p>+ RBC - Made From Scratch</p>
                     </AccordionItemTitle>
                     <AccordionItemBody>
                         <p></p>    
